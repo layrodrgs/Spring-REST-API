c52298b03e8e0107244431234af91b084a1690b5
Layanne Cantanhede <layrodrgs@gmail.com>
2020-05-26 17:57:12 -0300

    endpoint cadastro OrdemServico

--- osworks-api/src/main/java/com/algaworks/osworks/api/exceptionhandler/ApiExceptionHandler.java
+++ osworks-api/src/main/java/com/algaworks/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.algaworks.osworks.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,7 +36,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		Problema problema = new Problema();
 		problema.setStatus(((HttpStatus) status).value());
 		problema.setTitulo(ex.getMessage());
-		problema.setDataHora(LocalDateTime.now());
+		problema.setDataHora(OffsetDateTime.now());
 		
 		return handleExceptionInternal(ex, problema, new HttpHeaders(), (HttpStatus) status, request);
 	}
@@ -57,7 +58,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		problema.setStatus(status.value());
 		problema.setTitulo("Um ou mais campos estão inválidos. "
 				+ "Faça o preenchimento correto e tente novamente");
-		problema.setDataHora(LocalDateTime.now());
+		problema.setDataHora(OffsetDateTime.now());
 		problema.setCampos(campos);
 		return super.handleExceptionInternal(ex, problema, headers, status, request);
 	}
